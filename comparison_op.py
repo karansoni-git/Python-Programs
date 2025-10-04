725a02010461b109eb4425613f721f402d2c01e0
karan soni <karanparekh1011@gmail.com>
2025-10-04 21:51:46 +0530

    Add files via upload

---
+++ comparison_op.py
@@ -0,0 +1,11 @@
+# Comparison operators are used to compare two values or expressions. The result is always a Boolean value: True or False.
+
+x = 15
+y = 20
+
+print("x == y : " , x == y)
+print("x != y : " , x != y)
+print("x > y : " , x > y)
+print("x < y : " , x < y)
+print("x >= y : " , x >= y)
+print("x <= y : " , x <= y)
