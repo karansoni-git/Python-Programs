3a6e742f8c1dd4d85bb08f037e75a1433027266a
karan soni <karanparekh1011@gmail.com>
2025-10-04 21:51:13 +0530

    Add files via upload

---
+++ arithmetic_op.py
@@ -0,0 +1,11 @@
+# Arithmetic operators are used with numeric values to perform common mathematical operations
+a = 10
+b = 3
+
+print("a + b : " , a+b)
+print("a - b : " , a-b)
+print("a * b : " , a*b)
+print("a / b : " , a/b)
+print("a % b : " , a%b)
+print("a // b : " , a//b)
+print("a ** b : " , a**b)
