8136dba62211b45cc615a6286e7212fb18c7f1cb
karan soni <karanparekh1011@gmail.com>
2025-10-04 21:49:23 +0530

    Add files via upload

---
+++ __name__-module.py
@@ -0,0 +1,9 @@
+# first way to import the module here we import complete module
+# import name
+# name.sample() #accessing specific function or data from module
+
+# second way to import module here we import specific function or code to this module
+from name import sample,data
+sample()
+data()
+print("code is running")
