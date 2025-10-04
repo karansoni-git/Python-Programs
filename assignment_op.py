901d7da9e5772459ccc6e7953d7c86c8b3c5f12a
karan soni <karanparekh1011@gmail.com>
2025-10-04 21:51:37 +0530

    Add files via upload

---
+++ assignment_op.py
@@ -0,0 +1,18 @@
+# Assignment operators are used to assign values to variables
+a = 10
+print("a = 10")
+a+= 3
+print("a += 3 : ",a)
+a-=6
+print("a -= 6 : ",a)
+a*=2
+print("a *= 2 : ",a)
+a/=2
+print("a /= 2 : ",a)
+a**=2
+print("a **= 3 : ",a)
+a%=3
+print("a %= 3 : ",a)
+a +=20
+a//=2
+print("a //= 2 : ",a)
