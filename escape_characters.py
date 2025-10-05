16b7aea44ec1210e447c7b18a7ee35f523ad2fc7
karan soni <karanparekh1011@gmail.com>
2025-10-05 23:34:21 +0530

    Add files via upload

---
+++ escape_characters.py
@@ -0,0 +1,29 @@
+# \\	Backslash (\)
+print("this is \\backslash")
+
+# \'	Single quote (')
+print('single \'quote\'')
+
+# \"	Double quote (")
+print("double \"quote\"")
+
+# \n	New line
+print("pythong is\nrunning")
+
+# \t	Horizontal tab
+print("hit \tthe code")
+
+# \r	Carriage return
+print("sample \rtesting")
+
+# \b	Backspace
+print("hello\bboy")
+
+# \f	Form feed
+print("next\fpage")
+
+# \a	Bell/alert (makes a beep sound in some terminals)
+print("\amake a beep sound")
+
+# \v	Vertical tab
+print("give me \vsome space")
