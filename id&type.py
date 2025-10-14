65dc5ffa0708c66820444f1395d34d7d4fe8e24a
karan soni <karanparekh1011@gmail.com>
2025-10-14 21:45:13 +0530

    Add files via upload

---
+++ id&type.py
@@ -0,0 +1,21 @@
+# id(): Returns the unique identity of an object (its memory address). 
+# type(): Shows the data type of the variable. 
+# range(): Generates a sequence of numbers, commonly used in loops.
+
+num1 = 23
+num2 = 16.12
+str = "karan"
+list = [10,20,30]
+
+print("IDs of all variables :")
+print("ID of num1 : " , id(num1))
+print("ID of num2 : " , id(num2))
+print("ID of str : " , id(str))
+print("ID of list : " , id(list))
+
+print("\nTypes of all variables :")
+print("Type of num1 : " , type(num1))
+print("Type of num2 : " , type(num2))
+print("Type of str : " , type(str))
+print("Type of list : " , type(list))
+
