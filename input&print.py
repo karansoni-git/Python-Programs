c6d5cbf5deaaca39527d1879b0a717fec00a461a
karan soni <karanparekh1011@gmail.com>
2025-10-15 21:32:47 +0530

    Add files via upload

---
+++ input&print.py
@@ -0,0 +1,9 @@
+# => input(prompt) : 
+# The input() function is used to take input from the user during program~ execution.
+# prompt parameter : (Optional) A string message shown to the user before input
+
+name = input("What is your name? : ")
+age = input("How old are you? : ")
+
+print("hey" , name , "what's up , you are", age , "years old right")
+
