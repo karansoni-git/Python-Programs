cb9770cdc4d23d878e7de79131415c7254eb378a
karan soni <karanparekh1011@gmail.com>
2025-10-15 21:32:34 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,18 @@
+age = 20
+
+if age > 18:
+    print("you can vote")
+else:
+    print("you can't drive")
+
+grade = "A"
+if grade == "A":
+    print("First class")
+elif grade == "B":
+    print("Second class")
+elif grade == "C":
+    print("Third class")
+elif grade == "D":
+    print("looser class")
+else:
+    print("fail")
