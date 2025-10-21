75257e65d5a37fbac0ce20910c9e090b8829d332
karan soni <karanparekh1011@gmail.com>
2025-10-21 21:53:02 +0530

    Add files via upload

---
+++ logical_op.py
@@ -0,0 +1,13 @@
+# Logical operators are used to combine multiple conditional expressions. They return a Boolean value: True or False.
+
+x = 10
+y = 5
+
+# AND
+print(x > 5 and y < 10)   # True and True → True
+
+# OR
+print(x > 15 or y < 10)   # False or True → True
+
+# NOT
+print(not(x > 5))         # not(True) → False
