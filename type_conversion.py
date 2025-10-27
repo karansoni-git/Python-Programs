15cca31e6f9e7ac6787b552ed508dde53a531a7a
karan soni <karanparekh1011@gmail.com>
2025-10-27 23:51:24 +0530

    Add files via upload

---
+++ type_conversion.py
@@ -0,0 +1,24 @@
+int_var = 10
+float_var = 3.1415
+string_var = "123"
+boolean_var = True
+
+print("integer conversion : ")
+print("float to integer : " , int(float_var))
+print("string to integer : " , int(string_var))
+print("boolean to integer : " , int(boolean_var))
+
+print("\nfloat conversion :")
+print("integer to float : " , float(int_var))
+print("string to float : " , float(string_var))
+print("boolean to float : " , float(boolean_var))
+
+print("\nstring conversion :")
+print("integer to string : " , str(int_var))
+print("float to string : " , str(float_var))
+print("boolean to string : " , str(boolean_var))
+
+print("\nboolean conversion : ")
+print("integer to boolean : " , bool(int_var))
+print("float to boolean : " , bool(float_var))
+print("string to boolean : " , bool(string_var))
