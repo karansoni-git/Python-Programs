c2a38d1678cffdd658c74b2bd4b1d208ccb1babd
karan soni <karanparekh1011@gmail.com>
2025-10-31 21:53:01 +0530

    Delete fetchone().py

--- fetchone().py
+++
@@ -1,6 +0,0 @@
-import mysql.connector as myConn
-conn = myConn.connect(host = "localhost" , user = "root" , password = "" , database = "pytest")
-cursor = conn.cursor()
-cursor.execute("SELECT name,college FROM stud")
-oneRecord = cursor.fetchone()
-print(oneRecord)
