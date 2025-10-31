0de6998626dbbd68e02bc8e77d50062af8b0026a
karan soni <karanparekh1011@gmail.com>
2025-10-31 21:52:22 +0530

    Delete fetchmany().py

--- fetchmany().py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as myConn
-conn = myConn.connect(host = "localhost" , user = "root" , password = "" , database = "pytest")
-cursor = conn.cursor()
-cursor.execute("SELECT name,college FROM stud")
-rows = cursor.fetchmany(4)
-for r in rows:
-    print(r)
-
-conn.close()
