4b2843773ea13e4910d5957b3e16e5d16975f45b
karan soni <karanparekh1011@gmail.com>
2025-11-06 23:54:52 +0530

    Add files via upload

---
+++ exception handling/4_finally.py
@@ -0,0 +1,19 @@
+a = int(input("enter a : "))
+b = int(input("enter b : "))
+
+# The try block lets you test a block of code for errors.
+try:
+    d = a/b
+
+# The except block lets you handle the error.
+except:
+    print("somethong went wrong")
+    print("can not divide by 0")
+
+# The else block lets you execute code when there is no error.
+else:
+    print(f"division of {a}/{b} : {int(a/b)}")
+
+# The finally block, if specified, will be executed regardless if the try block raises an error or not.
+finally:
+    print("I am a finally block")
