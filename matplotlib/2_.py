753ae2ba291e8f23ae63bf58b62cdf6018bbcbba
karan soni <karanparekh1011@gmail.com>
2025-11-17 23:40:06 +0530

    Add files via upload

---
+++ matplotlib/2_.py
@@ -0,0 +1,9 @@
+import pylab
+pylab.figure(1)
+pylab.plot([1,2,3,4], [1,2,3,4])
+pylab.savefig("C:/programs/Python/Python Programs/matplotlib/first")
+pylab.figure(2)
+pylab.plot([1,4,2,3], [5,6,7,8])
+pylab.savefig("C:/programs/Python/Python Programs/matplotlib/second")
+
+pylab.show()
