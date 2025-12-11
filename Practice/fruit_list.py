f7ee99203c9cde17f612528fa26aae66a2bea8b3
karan soni <karanparekh1011@gmail.com>
2025-12-11 20:07:30 +0530

    Add files via upload

---
+++ Practice/fruit_list.py
@@ -0,0 +1,10 @@
+# fruits = input("Enter 5 fruits name : ")
+# fruit_list = fruits.split(" ")
+# print(type(fruit_list))
+# print(fruit_list)
+
+fruit_box = []
+for i in range(3):
+    name = input("Enter fruit name : ")
+    fruit_box.append(name)
+print(fruit_box)
