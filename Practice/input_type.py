8c53a081e7e7fcf1833052c57cfba77b6824753a
karan soni <karanparekh1011@gmail.com>
2025-12-12 21:18:39 +0530

    Add files via upload

---
+++ Practice/input_type.py
@@ -0,0 +1,3 @@
+# the value we get from user through input function is always in a string format whether it is a numer or string or any other data type value
+value = input("Enter something here : ")
+print(type(value))
