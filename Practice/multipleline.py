4305ea7e90bdfdc998c51503471d922500cbf6bc
karan soni <karanparekh1011@gmail.com>
2025-12-13 23:44:11 +0530

    Add files via upload

---
+++ Practice/multipleline.py
@@ -0,0 +1,9 @@
+# we can print multiple lines in python just by wrapping the text in triple single quotes or triple double quotes.
+print('''I tried to hide but something broke
+I tried to sing, couldn't hit the notes
+The words kept catching in my throat
+I tried to smile, I was suffocating though
+But here with you, I can finally breathe
+You say you're no good, but you're good for me
+I've been hoping to change, now I know we can change
+But I won't if you're not by my side''')
