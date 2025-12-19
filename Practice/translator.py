bd9d178d8ae6342d496d61c6a58baf4a3d3fe54b
karan soni <karanparekh1011@gmail.com>
2025-12-19 21:50:45 +0530

    Add files via upload

---
+++ Practice/translator.py
@@ -0,0 +1,9 @@
+words = {
+    "dog" : "kutta",
+    "watch" : "ghadi",
+    "clothes" : "kapde",
+    "window" : "khidki",
+}
+
+user_ask = input("Enter the word that you traslate : ")
+print(words.get(user_ask,"can't able to find!"))
